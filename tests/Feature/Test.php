3fc7139edc2d164063a889720ac975e49b357b7c
Axel Libori Roch <a.libori@gmail.com>
2024-12-10 09:33:43 +0100

    Update tests due to last changes

--- tests/Feature/Test.php
+++ tests/Feature/Test.php
@@ -7,9 +7,13 @@ namespace Feature;
 use Alibori\LaravelApiResourceGenerator\Console\GenerateApiResourceCommand;
 use Illuminate\Support\Facades\DB;
 
-// Test to check if the migration is running well
+// Test to check if the migrations are running well
 it('database users table has been created', function (): void {
     expect(DB::table('users')->get())->toBeEmpty();
+});
+
+it('database posts table has been created', function (): void {
+    expect(DB::table('posts')->get())->toBeEmpty();
 });
 
 // Test to check if GenerateApiResourceCommand is working well
@@ -31,7 +35,10 @@ it('can generate a resource for multiple models', function (): void {
 
     expect(file_exists(__DIR__.'/../../app/Http/Resources/UserResource.php'))->toBeTrue()
         ->and(file_get_contents(__DIR__.'/../../app/Http/Resources/UserResource.php'))->toContain('string $email')
-        ->and(file_exists(__DIR__.'/../../app/Http/Resources/PostResource.php'))->toBeTrue();
+        ->and(file_get_contents(__DIR__.'/../../app/Http/Resources/UserResource.php'))->not->toContain('string $title')
+        ->and(file_exists(__DIR__.'/../../app/Http/Resources/PostResource.php'))->toBeTrue()
+        ->and(file_get_contents(__DIR__.'/../../app/Http/Resources/PostResource.php'))->toContain('string $title')
+        ->and(file_get_contents(__DIR__.'/../../app/Http/Resources/PostResource.php'))->not->toContain('string $name');
 
     unlink(__DIR__.'/../../app/Http/Resources/UserResource.php');
     unlink(__DIR__.'/../../app/Http/Resources/PostResource.php');
